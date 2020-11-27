db50aca5e2803ec810420477a061e86b18774cdf
Merlin Webster <mjftwebster@gmail.com>
2020-11-27 18:05:01 +0000

    Added REST endpoint and tests semiprime that takes a batch

--- lib/semiprimes_web/controllers/api_controller.ex
+++ lib/semiprimes_web/controllers/api_controller.ex
@@ -2,10 +2,23 @@ defmodule SemiprimesWeb.APIController do
   use SemiprimesWeb, :controller
   alias Semiprimes.Math
 
+  # Single number to check if semiprime
   def semiprime(conn, %{"number" => number}) do
+    json(conn, semiprime_answer(number))
+  end
+
+  # Batch of numbers to check if semiprime
+  def semiprime(conn, %{"batch" => batch}) do
     json(conn, %{
-      "number" => number,
-      "is_semiprime" => Math.is_semiprime?(number)
+      "batch" => Enum.map(batch, &semiprime_answer/1)
     })
+  end
+
+  # Helper to ensure consistent return format
+  defp semiprime_answer(num) do
+    %{
+      "number" => num,
+      "is_semiprime" => Math.is_semiprime?(num)
+    }
   end
 end
--- test/semiprimes_web/controllers/api_controller_test.exs
+++ test/semiprimes_web/controllers/api_controller_test.exs
@@ -15,7 +15,32 @@ defmodule SemiprimesWeb.APIControllerTest do
     number = 101
     resp = post(conn, Routes.api_v1_api_path(conn, :semiprime), number: number)
 
+    # Only checking answer is expected not correct, Math.is_semiprime?/1 has its own tests
     expected = Semiprimes.Math.is_semiprime?(number)
     assert %{"number" => ^number, "is_semiprime" => ^expected} = json_response(resp, 200)
+  end
+
+  test "POST to :semiprime action, correct response format with a batch of numbers", %{conn: conn} do
+    numbers = [9, 10, 11]
+    resp = post(conn, Routes.api_v1_api_path(conn, :semiprime), batch: numbers)
+
+    # Check batch in response, and batch is non-empty list
+    assert %{"batch" => [_ | _] } = json_response(resp, 200)
+  end
+
+  test "POST to :semiprime action works with a batch of numbers", %{conn: conn} do
+    numbers = [9, 10, 11, 15, 19, 21, 35]
+
+    # Only checking answer is expected not correct, Math.is_semiprime?/1 has its own tests
+    expected_is_semiprimes = Enum.map(numbers, &Semiprimes.Math.is_semiprime?/1)
+    expected_items =
+      for {number, is_semiprime} <- Enum.zip(numbers, expected_is_semiprimes) do
+        %{"number" => number, "is_semiprime" => is_semiprime}
+      end
+    expected = %{"batch" => expected_items}
+
+    resp = post(conn, Routes.api_v1_api_path(conn, :semiprime), batch: numbers)
+
+    assert expected == json_response(resp, 200)
   end
 end
